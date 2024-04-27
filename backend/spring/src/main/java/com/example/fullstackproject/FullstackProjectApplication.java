b19fb66d26f9f545e8b69e923e4167605ec8e663
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2024-04-27 14:00:06 -0300

    initial

---
+++ backend/spring/src/main/java/com/example/fullstackproject/FullstackProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackProjectApplication.class, args);
+	}
+
+}
