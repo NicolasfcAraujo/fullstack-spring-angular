8339d67b5bb672921358ffe3035ee05a8969af08
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2024-05-01 10:52:52 -0300

    Main pages created

--- frontend/angular/src/app/app.routes.ts
+++ frontend/angular/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PanelComponent } from './pages/panel/panel.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { TeamComponent } from './pages/team/team.component';
+import { AddMemberComponent } from './pages/add-member/add-member.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "" , component: PanelComponent, title: "HR Web | Panel" },
+  { path: "profile", component: ProfileComponent, title: "HR Web | Profile" },
+  { path: "team", component: TeamComponent, title: "HR Web | Team" },
+  { path: "add-member", component: AddMemberComponent, title: "HR Web | Add Member" }
+];
