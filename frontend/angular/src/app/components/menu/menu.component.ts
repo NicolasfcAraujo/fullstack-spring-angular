9826d18364178741df1b5ee77377c532ae165e3b
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2024-05-01 13:22:08 -0300

    menu is working

--- frontend/angular/src/app/components/menu/menu.component.ts
+++ frontend/angular/src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
