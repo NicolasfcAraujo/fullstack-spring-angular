2d49b4b9a71d6a234fbe112d63368df1646eb35a
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2024-05-02 13:26:30 -0300

    auth guard created

---
+++ frontend/angular/src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  authorize: boolean = false
+
+  constructor() { }
+
+  login() {
+
+  }
+
+  logout() {
+    
+  }
+}
