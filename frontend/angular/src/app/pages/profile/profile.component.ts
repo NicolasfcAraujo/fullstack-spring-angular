7a88e8cd85119911c1208ed7073721091482922a
NICOLAS ARAUJO <nicolasfcaraujo@gmail.com>
2024-05-03 13:57:58 -0300

    user info on profile

--- frontend/angular/src/app/pages/profile/profile.component.ts
+++ frontend/angular/src/app/pages/profile/profile.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  
+  profileForm = new FormGroup({
+    fullName: new FormControl(),
+    email: new FormControl(),
+    document: new FormControl(),
+    password: new FormControl(),
+    role: new FormControl(),
+    department: new FormControl(),
+    type: new FormControl("COMMON")
+  })
+
+  constructor(){
+    this.profileForm.patchValue({
+      fullName: localStorage.getItem("fullName"),
+      email: localStorage.getItem("email"),
+      document: localStorage.getItem("document"),
+      password: localStorage.getItem("password"),
+      role: localStorage.getItem("role"),
+      department: localStorage.getItem("department"),
+      type: localStorage.getItem("type")
+    })
+  }
 }
